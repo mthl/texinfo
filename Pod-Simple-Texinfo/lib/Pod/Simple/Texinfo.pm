b3c31afc6ffb65ff39d9dab4cefcd4cc4d96f9cb
Gavin Smith <gavinsmith0123@gmail.com>
2016-10-30 16:08:48 +0000

    update pod2texi
    git-svn-id: svn://127.0.0.1/svn_repo/texinfo/trunk@7481 39fee189-59d7-47db-b5d4-205258b72aed

--- Pod-Simple-Texinfo/lib/Pod/Simple/Texinfo.pm
+++ Pod-Simple-Texinfo/lib/Pod/Simple/Texinfo.pm
@@ -292,7 +292,7 @@ sub _reference_to_text_in_texi($)
 {
   my $texinfo = shift;
   my $tree = parse_texi_text(undef, $texinfo);
-  Texinfo::Structuring::reference_to_arg_in_tree(undef, $tree);
+  Texinfo::Transformations::reference_to_arg_in_tree(undef, $tree);
   return Texinfo::Convert::Texinfo::convert($tree);
 }  
 
