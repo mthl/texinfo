77df23f2ac454ad12e3bcc927be3856763b489e9
Mathieu Lirzin <mthl@gnu.org>
2017-06-13 03:47:11 +0200

    build: Use 'rollup-plugin-eslint'

--- js/build-aux/rollup-kawa.js
+++ js/build-aux/rollup-kawa.js
@@ -1,12 +1,18 @@
 /* Rollup plugins.  */
-import buble from 'rollup-plugin-buble';
+import buble from "rollup-plugin-buble";
+import eslint from "rollup-plugin-eslint";
 
 export default {
-  entry: 'src/index.js',
-  dest: 'examples/kawa/kawa-ebook.js',
-  format: 'iife',
-  sourceMap: 'inline',
+  entry: "src/index.js",
+  dest: "examples/kawa/kawa-ebook.js",
+  format: "iife",
+  sourceMap: "inline",
   plugins: [
-    buble()
+    eslint ({
+      configFile: "build-aux/eslint.json",
+      formatter: "unix",
+      include: "src/*"
+    }),
+    buble ()
   ]
 };
--- js/build-aux/rollup.js
+++ js/build-aux/rollup.js
@@ -1,12 +1,18 @@
 /* Rollup plugins.  */
-import buble from 'rollup-plugin-buble';
+import buble from "rollup-plugin-buble";
+import eslint from "rollup-plugin-eslint";
 
 export default {
-  entry: 'src/index.js',
-  dest: 'build/info.js',
-  format: 'iife',
-  sourceMap: 'inline',
+  entry: "src/index.js",
+  dest: "build/info.js",
+  format: "iife",
+  sourceMap: "inline",
   plugins: [
-    buble()
+    eslint ({
+      configFile: "build-aux/eslint.json",
+      formatter: "unix",
+      include: "src/*"
+    }),
+    buble ()
   ]
 };
