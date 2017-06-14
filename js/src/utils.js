e78eeb9cc890e51190c86a5ca53fdfe20b0a52ff
Mathieu Lirzin <mthl@gnu.org>
2017-06-14 15:00:38 +0200

    utils: Avoid conversion from NodeList to Array in 'navigation_links'

--- js/src/utils.js
+++ js/src/utils.js
@@ -71,16 +71,19 @@ href_hash (href)
 export function
 navigation_links (content)
 {
-  let links = Array.from (content.querySelectorAll ("footer a"));
-
+  let links = content.querySelectorAll ("footer a");
+  let res = {};
   /* links have the form MAIN_FILE.html#FRAME-ID.  For convenience
-     we only store FRAME-ID.  */
-  return links.reduce ((acc, link) => {
-    let nav_id = navigation_links.dict[link.getAttribute ("accesskey")];
-    if (nav_id)
-      acc[nav_id] = href_hash (link.getAttribute ("href"));
-    return acc;
-  }, {});
+     only store FRAME-ID.  */
+  for (let i = 0; i < links.length; i += 1)
+    {
+      let link = links[i];
+      let nav_id = navigation_links.dict[link.getAttribute ("accesskey")];
+      if (nav_id)
+        res[nav_id] = href_hash (link.getAttribute ("href"));
+    }
+
+  return res;
 }
 
 /* Dictionary associating an 'accesskey' property to its navigation id.  */
