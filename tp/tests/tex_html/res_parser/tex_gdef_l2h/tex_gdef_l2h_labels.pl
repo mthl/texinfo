96f6aa002cc8faa7aeb35c0d3664dea33e638b0d
Patrice Dumas <pertusus@free.fr>
2020-11-12 20:40:33 +0100

    Remove tex_gdef test files that should not be in the final result directory

--- tp/tests/tex_html/res_parser/tex_gdef_l2h/tex_gdef_l2h_labels.pl
+++ tp/tests/tex_html/res_parser/tex_gdef_l2h/tex_gdef_l2h_labels.pl
@@ -1,11 +1,11 @@
-# LaTeX2HTML 2020.2 (Released July 1, 2020)
+# LaTeX2HTML
 # Associate labels original text with physical files.
 
 
 1;
 
 
-# LaTeX2HTML 2020.2 (Released July 1, 2020)
+# LaTeX2HTML
 # labels from external_latex_labels array.
 
 
