f80278b52af612a974a272c52a56dabd07e4d12d
Patrice Dumas <pertusus@free.fr>
2020-11-12 15:59:30 +0100

    * tp/tests/tex_html/tex_gdef.texi: test with \gdef in @tex block from Christopher Dimech.

---
+++ tp/tests/tex_html/res_parser/tex_gdef_l2h/tex_gdef-l2h_cache.pm
@@ -0,0 +1,28 @@
+
+$l2h_cache_key = q/  \gdef\Desig{\buildrel \rm def \over \equiv}/;
+$l2h_cache{$l2h_cache_key} = q|  |;
+
+$l2h_cache_key = q/$$ \Desig
+$$/;
+$l2h_cache{$l2h_cache_key} = q|<BR><P></P>
+<DIV CLASS="displaymath">
+<!-- MATH
+ \begin{displaymath}
+\buildrel \rm def \over \equiv
+\end{displaymath}
+ -->
+
+<IMG STYLE="height: 3.12ex; vertical-align: -0.12ex; " SRC="tex_gdef_2.svg"
+ ALT="$\displaystyle \buildrel \rm def \over \equiv
+$">
+</DIV>
+<BR CLEAR="ALL">
+<P></P>|;
+
+$l2h_cache_key = q/$\Desig$/;
+$l2h_cache{$l2h_cache_key} = q|<!-- MATH
+ $\buildrel \rm def \over \equiv$
+ -->
+<SPAN CLASS="MATH"><IMG STYLE="height: 3.12ex; vertical-align: -0.12ex; " SRC="tex_gdef_1.svg"
+ ALT="$\buildrel \rm def \over \equiv $"></SPAN>|;
+1;
---
+++ tp/tests/tex_html/res_parser/tex_gdef_l2h/tex_gdef_l2h_labels.pl
@@ -0,0 +1,13 @@
+# LaTeX2HTML 2020.2 (Released July 1, 2020)
+# Associate labels original text with physical files.
+
+
+1;
+
+
+# LaTeX2HTML 2020.2 (Released July 1, 2020)
+# labels from external_latex_labels array.
+
+
+1;
+
